80bff111946ca861cd717b0d7e96683ab0add403
gabrielagi <gabrielagi503@gmail.com>
2020-10-01 17:16:12 -0300

    saque componentes que no iban

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,7 +2,6 @@
 require('./bootstrap');
 import 'bootstrap-css-only/css/bootstrap.min.css';
 import 'mdbvue/lib/css/mdb.min.css';
-import '../css/style.css';
 
 window.Vue = require('vue');
 
@@ -12,64 +11,10 @@ import App from './views/App';
 import router from './router';
 import store from './store';
 
-// Mixins
-import alertMixin from './mixins/alertMixin';
-
 // Waves
 import Waves from 'vue-waves-effect';
 import 'vue-waves-effect/dist/vueWavesEffect.css';
 Vue.use(Waves);
-
-// Mixins
-Vue.mixin(alertMixin);
-
-/*-----------------------------------------------------------
-
-  Components
-
------------------------------------------------------------*/
-/*-----------------------------------------------------------
-  Navigation
------------------------------------------------------------*/
-// Navbar
-Vue.component('navbar', require('./components/Navbar.vue').default);
-
-/*-----------------------------------------------------------
-  Forms
------------------------------------------------------------*/
-// Input
-Vue.component('v-input', require('./components/forms/Input.vue').default);
-
-/*-----------------------------------------------------------
-  Charts
------------------------------------------------------------*/
-// Line
-Vue.component('line-chart', require('./components/charts/LineChart.vue').default);
-// Scatter
-Vue.component('scatter-chart', require('./components/charts/ScatterChart.vue').default);
-// Data Table
-Vue.component('data-table', require('./components/Table.vue').default);
-
-/*-----------------------------------------------------------
-  User dashboard
------------------------------------------------------------*/
-// Dashboard navigation
-Vue.component('dashboard-navigation', require('./components/dashboard/navigation/Navigation.vue').default);
-// Title
-Vue.component('dashboard-title', require('./components/dashboard/Title.vue').default);
-// Subtite
-Vue.component('dashboard-subtitle', require('./components/dashboard/SubTitle.vue').default);
-
-
-/*-----------------------------------------------------------
-  Loading status
------------------------------------------------------------*/
-// Spinner
-Vue.component('spinner', require('./components/loading/Spinner.vue').default);
-// Loading Overlay
-Vue.component('loading-overlay', require('./components/loading/Overlay.vue').default);
-// Loading Overlay
-Vue.component('loading-dots', require('./components/loading/Dots.vue').default);
 
 /*-----------------------------------------------------------
 
