490e25f23d95ac2fdc005525c19f9d0246016800
gabrielagi <gabrielagi503@gmail.com>
2020-10-10 13:14:19 -0300

    agregué el subtitulo para el dashboard

---
+++ resources/js/components/dashboard/SubTitle.vue
@@ -0,0 +1,21 @@
+<template>
+  <h2 class="dashboard-subtitle">{{title}}</h2>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
+
+<style scoped>
+.dashboard-subtitle {
+  font-size: 1.5em;
+  color: #454545;
+}
+</style>
