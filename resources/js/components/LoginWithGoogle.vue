adfaa373d63bceb2e6cb7ff5752deab6a921513a
manuelguido <manuelguido.m@gmail.com>
2020-10-10 13:02:21 -0300

    agrego componente de login con google

---
+++ resources/js/components/LoginWithGoogle.vue
@@ -0,0 +1,11 @@
+<template>
+  <!-- /.Login button -->
+  <div class="form-group mt-0 mb-0 text-center">
+    <!-- <hr class="my-5"> -->
+    <p class="h6 black-alpha-40 my-4">o ingresa con</p>
+    <a href="/api/authorize/google" class="btn btn-danger btn-block">
+      <i class="fab fa-google mr-3"></i>
+      Google
+    </a>
+  </div>
+</template>
