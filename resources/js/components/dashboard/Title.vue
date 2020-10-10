05cfa119400a1560e64f6a7d91b883d8b1c4811d
gabrielagi <gabrielagi503@gmail.com>
2020-10-10 13:09:48 -0300

    agregué: titulo y subtitulo para el dashboard

---
+++ resources/js/components/dashboard/Title.vue
@@ -0,0 +1,31 @@
+<template>
+  <div v-if="full" class="row">
+    <div class="title-col col-12">
+      <h1 class="dashboard-title mb-4">{{text}}</h1>
+    </div>
+  </div>
+  <h1 v-else class="dashboard-title mb-4">{{text}}</h1>
+</template>
+
+<script>
+export default {
+  props: {
+    text: {
+      type: String,
+      default: ''
+    },
+    full: {
+      type: Boolean,
+      default: false
+    }
+  }
+}
+</script>
+
+<style scoped>
+.dashboard-title {
+  font-size: 2em;
+  font-weight: 400;
+  color: #777;
+}
+</style>
