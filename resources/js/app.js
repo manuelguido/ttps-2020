9f17ef6767ed6cdfe1de11aebf3ef4826426f7dd
gabrielagi <gabrielagi503@gmail.com>
2020-10-10 14:45:52 -0300

    agregué componenetes a app.js y estilos

--- resources/js/app.js
+++ resources/js/app.js
@@ -26,7 +26,9 @@ Vue.mixin(alertMixin);
 Vue.mixin(assetMixin);
 
 /*-----------------------------------------------------------
+
   Components
+
 -----------------------------------------------------------*/
 /*-----------------------------------------------------------
   Navigation
@@ -41,7 +43,40 @@ Vue.component('navbar', require('./components/Navbar.vue').default);
 Vue.component('v-input', require('./components/forms/Input.vue').default);
 
 /*-----------------------------------------------------------
+  Charts
+-----------------------------------------------------------*/
+// Line
+Vue.component('line-chart', require('./components/charts/LineChart.vue').default);
+// Scatter
+Vue.component('scatter-chart', require('./components/charts/ScatterChart.vue').default);
+// Data Table
+Vue.component('data-table', require('./components/Table.vue').default);
+
+/*-----------------------------------------------------------
+  User dashboard
+-----------------------------------------------------------*/
+// Dashboard navigation
+Vue.component('dashboard-navigation', require('./components/dashboard/navigation/Navigation.vue').default);
+// Title
+Vue.component('dashboard-title', require('./components/dashboard/Title.vue').default);
+// Subtite
+Vue.component('dashboard-subtitle', require('./components/dashboard/SubTitle.vue').default);
+
+
+/*-----------------------------------------------------------
+  Loading status
+-----------------------------------------------------------*/
+// Spinner
+Vue.component('spinner', require('./components/loading/Spinner.vue').default);
+// Loading Overlay
+Vue.component('loading-overlay', require('./components/loading/Overlay.vue').default);
+// Loading Overlay
+Vue.component('loading-dots', require('./components/loading/Dots.vue').default);
+
+/*-----------------------------------------------------------
+
   Vue instance
+
   -----------------------------------------------------------*/
 new Vue({
   el: '#app',
