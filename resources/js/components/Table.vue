4acec24ca264df8c4833acfaa97894b8682bbc9a
manuelguido <manuelguido.m@gmail.com>
2020-10-10 13:02:54 -0300

    agrego componente de tabla para listados

---
+++ resources/js/components/Table.vue
@@ -0,0 +1,50 @@
+<template>
+  <mdb-datatable
+    striped
+    bordered
+    arrows
+    reactive
+    data="http://127.0.0.1:8000/api/patient/index"
+    :time="3000"
+    :tfoot="false"
+  />
+</template>
+
+<script>
+  import { mdbDatatable } from 'mdbvue';
+  export default {
+    name: 'DatatablePage',
+    components: {
+      mdbDatatable
+    },
+    props: {
+      columns: {
+        type: Array,
+      },
+      rows: {
+        type: Array,
+      }
+    },
+    // created () {
+    //   this.setColumns();
+    //   this.setRows();
+    // },
+    data() {
+      return {
+        data: {
+          columns: [],
+          rows: []
+        }
+      }
+    },
+    methods: {
+      setColumns () {
+        this.data.columns = this.columns;
+      },
+
+      setRows () {
+        this.data.rows = this.rows;
+      }
+    }
+  }
+</script>
