85be46ba746662a06ecc4f3a2d0834121ca0c8de
manuelguido <manuelguido.m@gmail.com>
2020-10-16 13:37:31 -0300

    agregué seed para los estados del paciente

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -17,6 +17,7 @@ class DatabaseSeeder extends Seeder
             UsersTableSeeder::class,
             MedicalEnsuranceTableSeeder::class,
             RoomAndBedsTableSeeder::class,
+            PatientStatesTableSeeder::class,
         ]);
     }
 }
---
+++ database/seeds/PatientStatesTableSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\PatientState;
+
+class PatientStatesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        function new_patient_state($name)
+        {
+            $state = new PatientState;
+            $state->state = $name;
+            $state->save();
+            return $state;
+        }
+
+        new_patient_state(PatientState::STATE_HOSPITALIZED);
+        new_patient_state(PatientState::STATE_DISCHARGED);
+        new_patient_state(PatientState::STATE_DEATH);
+    }
+}
