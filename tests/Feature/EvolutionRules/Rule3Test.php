ac3a7794ad89902c37269bad02b76b2e04c73363
manuelguido <manuelguido.m@gmail.com>
2020-12-16 02:13:33 -0300

    test de regla 3 (Frecuencia respiratoria mayor al parámetro)

---
+++ tests/Feature/EvolutionRules/Rule3Test.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\RuleSettings;
+use App\Evolution;
+
+
+class Rule3 extends TestCase
+{
+    /**
+     * Test de regla 3: Ver que la Frecuencia Respiratoria es mayor a al parámetro.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        // Obtener configuración de reglas
+        $ruleSettings = RuleSettings::find(1);        
+        
+        // Creación de evolución
+        $evolution = factory(Evolution::class)->create();
+
+        /**
+         * Testeo de regla cuando el paciente frecuencia respiratoria mayor al parámetro
+         */
+        $evolution->breathing_rate = 21;
+        $ruleSettings->breathing_rate = 20;
+        $ruleCondition = $ruleSettings->analizeRule3($evolution);
+        $this->assertTrue($ruleCondition);
+
+        /**
+         * Testeo de regla cuando el paciente frecuencia respiratoria menor al parámetro
+         */
+        $evolution->breathing_rate = 17;
+        $ruleSettings->breathing_rate = 20;
+        $ruleCondition = $ruleSettings->analizeRule3($evolution);
+        $this->assertFalse($ruleCondition);
+
+        /**
+         * Borrado de información de test en DB
+         */
+        $evolution->delete();
+    }
+}
